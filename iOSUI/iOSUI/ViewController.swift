8ba3e2557725d95afc296b34bc462e2292e7f9f0
Nithin <nithin@mobomo.com>
2020-11-08 16:40:13 +0530

    Initial commit

---
+++ iOSUI/iOSUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSUI
+//
+//  Created by Nithin Nizam on 08/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
