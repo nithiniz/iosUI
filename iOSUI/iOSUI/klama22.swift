3cfd45ab140cf2b3189329ba143b786545c916eb
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2020-12-28 12:51:18 -0800

    Added more comments

--- iOSUI/iOSUI/klama22.swift
+++ iOSUI/iOSUI/klama22.swift
@@ -15,3 +15,20 @@ import Foundation
 //  Created by Utsav Shrestha on 12/27/20.
 //  Copyright © 2020 Nithin. All rights reserved.
 //
+
+
+//
+//  klama22.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/27/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
+
+//
+//  klama22.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/27/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
