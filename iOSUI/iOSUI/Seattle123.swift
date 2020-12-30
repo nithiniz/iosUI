076a96817fb34c15b39cdcc5717a238011c45542
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2020-12-30 11:27:47 -0800

    Added Seattle file

---
+++ iOSUI/iOSUI/Seattle123.swift
@@ -0,0 +1,31 @@
+//
+//  Seattle123.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/30/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
+
+import Foundation
+
+//
+//  Seattle123.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/30/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
+//
+//  Seattle123.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/30/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
+//
+//  Seattle123.swift
+//  iOSUI
+//
+//  Created by Utsav Shrestha on 12/30/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
