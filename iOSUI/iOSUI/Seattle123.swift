00d181536d0c63ca8c35363c37802e5312d9f4e8
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2020-12-30 11:43:55 -0800

    Added few func in Seattle file

--- iOSUI/iOSUI/Seattle123.swift
+++ iOSUI/iOSUI/Seattle123.swift
@@ -29,3 +29,21 @@ import Foundation
 //  Created by Utsav Shrestha on 12/30/20.
 //  Copyright © 2020 Nithin. All rights reserved.
 //
+
+
+func test_123(){
+//    let app = XCUIApplication()
+//    app.launch()
+//
+//    //XCUIApplication().textFields["Button1"]
+//    //XCUIApplication().textFields["Button1"].typeText("Vegetables")
+//    app.buttons["Button1"].tap()
+//    app.navigationBars["iOSUI.SecondView"].buttons["Back"].tap()
+//
+//    app.buttons["Button2"].tap()
+//    }
+}
+
+func test_234(){
+    
+}
